07c3f5a7addac8c3c1919da1e441543ba2de32b3
Roberto Cabrera <roberto@webbits.dev>
2025-01-07 19:32:15 -0500

    Initial Commit

---
+++ FetchRecipeApp/FetchRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeAppApp.swift
+//  FetchRecipeApp
+//
+//  Created by Roberto Cabrera on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
